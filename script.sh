73a02e7bc572649a55f107b6ea84cf7dce00f981
aristidejou <106462048+aristidejou@users.noreply.github.com>
2023-02-16 17:23:36 -0600

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,18 @@
+          echo 3. where is the android SDK Root?---------------------------
+          echo $ANDROID_SDK_ROOT
+          echo 
+          echo 4. where are the selinium jars?--------------------------
+          echo $SELENIUM_JAR_PATH
+          echo
+          echo 5. what is the workspace location?--------------------------
+          echo $RUNNER_WORKSPACE
+          echo
+          echo 6. who is running the script?--------------------------
+          whoami
+          echo
+          echo 7.how is the disc laid out?--------------------------
+          df
+          echo
+          echo 8. what is the environment variables?--------------------------
+          env
+          
